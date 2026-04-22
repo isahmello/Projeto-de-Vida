0c6e7c0ef7d4ace5316fd63adb071cfdce01d7f5
isahmello <isadora.mello.santos@escola.pr.gov.br>
2026-04-22 16:01:10 -0300

    l

---
+++ main.js
@@ -0,0 +1,10 @@
+const botoes=document.querySelectorAll(".botao");
+console.log(botoes);
+
+for (let i=0; i<= botoes.length; i++){
+
+    botoes[i].onclick=function() {
+        botoes[i].classList.add("ativo");
+    }
+   console.log(botoes[i]);
+ }
